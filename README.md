297596331ea7cd8b59e2cd5bfaa658463904f8b3
fernandog301 <fernandog301g@gmail.com>
2024-04-04 16:42:51 -0700

    test

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+Fernando Gutierrez
+4/4/2024
+Description: redo this tipping calculator with styling and making it functional.
+peer reviewed by:
+Peer reviewed description.
+
+
+
+
+
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/Components/MainComponents.tsx
+++ src/app/Components/MainComponents.tsx
@@ -1,11 +1,148 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import dollarIcon from '../../app/assets/images/icon-dollar.svg'
+import personIcon from '../../app/assets/images/icon-person.svg'
+import logo from '../../app/assets/images/logo.svg'
 
 const MainComponents = () => {
-  return (
-    <div>
-      <h1>hey</h1>
-    </div>
-  )
+
+    const [billAmount, setBillAmount] = useState<string>('');
+    const [tipPercentage, setTipPercentage] = useState<string>('');
+    const [peopleAmount, setPeopleAmount] = useState<string>('');
+    const [tipAmount, setTipAmount] = useState<string>('0.00');
+    const [totalAmount, setTotalAmount] = useState<string>('0.00');
+    const [errorClass, setErrorClass] = useState<string>('');
+
+
+    // useEffect to handle calculations
+    useEffect(() => {
+        let tips: string;
+
+        const calculateTipAmount = () => {
+            tips = (Number(billAmount) * Number(tipPercentage) / Number(peopleAmount)).toFixed(2);
+            return tips;
+        }
+
+        const calculateTotalAmount = () => {
+            return ((Number(tips) + Number(billAmount)) / Number(peopleAmount)).toFixed(2);
+        }
+
+        if (billAmount && tipPercentage && peopleAmount) {
+            setTipAmount(calculateTipAmount());
+            setTotalAmount(calculateTotalAmount());
+        }
+
+        if (peopleAmount === '0') {
+            
+        }
+    }, [billAmount, tipPercentage, peopleAmount]);
+
+
+    // helper function to update tipPercentage useState
+    const handleTipPercentage = (percentage: string) => {
+        setTipPercentage((Number(percentage) / 100).toFixed(2));
+        console.log(tipPercentage);
+    }
+
+
+    // helper function to handle resetting all values
+    const handleReset = () => {
+        setBillAmount('');
+        setTipPercentage('');
+        setPeopleAmount('');
+        setTipAmount('0.00');
+        setTotalAmount('0.00');
+
+        const customTip = document.getElementById('customTip') as HTMLInputElement;
+
+        if (customTip) {
+            customTip.value = '';
+            customTip.placeholder = 'Custom';
+        }
+    }
+
+
+    return (
+        <div className="grid justify-center items-center">
+            {/* Splitter logo */}
+            <div className='mt-12 mb-10 lg:mt-40 lg:mb-20'>
+                <img className='mx-auto' src={logo.src} alt="logo" />
+            </div>
+
+            {/* White Background Container */}
+            <div className='bg-white lg:mx-[260px] rounded-t-2xl lg:rounded-3xl'>
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+
+                    {/* Column One */}
+                    <div className='m-8 mb-none lg:m-12 lg:mr-8'>
+
+                        {/* Bill Input Field */}
+                        <div className='grid grid-flow-row mb-10 lg:mb-11'>
+                            <label className='mb-3  text-grayishCyan' htmlFor="bill">Bill</label>
+                            <div className='flex justify-between items-center bg-veryLightGrayishCyan hover:border-strongCyan focus:outline-strongCyan hover:border-2 rounded-lg px-4 cursor-pointer'>
+                                <img src={dollarIcon.src} alt="dollar icon" />
+                                <input value={billAmount} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBillAmount(e.target.value)} name='bill' id='bill' className='w-full bg-transparent text-2xl outline-none text-veryDarkCyan text-right h-12 cursor-pointer' type="text" placeholder='0' />
+                            </div>
+                        </div>
+
+                        {/* Button field */}
+                        <div className='grid grid-cols-2 lg:grid-cols-3 justify-center gap-4 mb-10 lg:mb-11'>
+                            <h1 className='col-span-2 lg:col-span-3  text-grayishCyan'>Select Tip %</h1>
+                            <button onClick={() => handleTipPercentage('5')} className='bg-veryDarkCyan  text-white hover:bg-strongCyan hover:text-veryDarkCyan active:bg-strongCyan active:text-veryDarkCyan text-xl  h-12 rounded-lg '>5%</button>
+                            <button onClick={() => handleTipPercentage('10')} className='bg-veryDarkCyan text-white hover:bg-strongCyan hover:text-veryDarkCyan text-xl  h-12 rounded-lg '>10%</button>
+                            <button onClick={() => handleTipPercentage('15')} className='bg-veryDarkCyan text-white hover:bg-strongCyan hover:text-veryDarkCyan text-xl  h-12 rounded-lg '>15%</button>
+                            <button onClick={() => handleTipPercentage('25')} className='bg-veryDarkCyan text-white hover:bg-strongCyan hover:text-veryDarkCyan text-xl  h-12 rounded-lg '>25%</button>
+                            <button onClick={() => handleTipPercentage('50')} className='bg-veryDarkCyan text-white hover:bg-strongCyan hover:text-veryDarkCyan text-xl  h-12 rounded-lg '>50%</button>
+                            <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleTipPercentage(e.target.value)} name='customTip' id='customTip' className='text-2xl bg-veryLightGrayishCyan text-veryDarkCyan text-right hover:border-strongCyan focus:outline-strongCyan hover:border-2  px-4 rounded-lg cursor-pointer' type="text" placeholder={tipPercentage === '0' || '5' || '10' || '25' || '50' ? 'Custom' : ''} />
+                        </div>
+
+                        {/* Number of People Input Field */}
+                        <div className='grid grid-flow-row '>
+                            <label className='lg:mb-2 text-grayishCyan' htmlFor="people">Number of People</label>
+                            <div className='flex justify-between items-center bg-veryLightGrayishCyan hover:border-strongCyan focus:outline-strongCyan hover:border-2 rounded-lg px-4 cursor-pointer'>
+                                <img src={personIcon.src} alt="dollar icon" />
+                                <input value={peopleAmount} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPeopleAmount(e.target.value)} name='people' id='people' className='w-full bg-transparent text-2xl outline-none text-veryDarkCyan text-right h-12 cursor-pointer invalid:outline-orange-400' type="text" placeholder='1' />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Column Two - Dark Background Div */}
+                    <div className='bg-veryDarkCyan m-6 lg:m-8 p-6 lg:pb-0 lg:ml-[14px] lg:p-10 lg:pt-16 rounded-xl '>
+                        <div className='grid grid-cols-2'>
+                            {/* Tip Amount */}
+                            <div className=''>
+                                <h1 className='text-white'>Tip Amount</h1>
+                                <p className='text-lightGrayishCyan'>/ person</p>
+                            </div>
+                            <div className='grid justify-end items-center'>
+                                <h1 className='text-3xl text-strongCyan'>${tipAmount}</h1>
+                            </div>
+
+                            {/* Spacer Div */}
+                            <div className='mb-10 lg:mb-14 col-span-2'></div>
+
+                            {/* Total Amount */}
+                            <div className=''>
+                                <h1 className='text-white'>Total</h1>
+                                <p className='text-lightGrayishCyan'>/ person</p>
+                            </div>
+                            <div className='flex justify-end items-center'>
+                                <h1 className='text-3xl text-strongCyan'>${totalAmount}</h1>
+                            </div>
+
+                            {/* Spacer Div */}
+                            <div className='mb-10 lg:mb-32'></div>
+
+                            {/* Reset Button */}
+                            <div className=' col-span-2'>
+                                <button onClick={handleReset} className={`w-full ${billAmount == '' && tipPercentage == '' && peopleAmount == '' ? 'bg-resetBackground' : 'bg-buttonBackground'}  text-veryDarkCyan hover:bg-buttonBackground hover:text-veryDarkCyan text-xl h-12 rounded-lg `}>RESET</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MainComponents
